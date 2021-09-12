75f20a407c89f608d01f75913521e954c764d4b9
gpucce <g.puccetti92@gmail.com>
2021-09-12 23:47:49 +0200

    reinit Repo

--- JUtils/src/Util.jl
+++
@@ -1,7 +0,0 @@
-function specificity(tar, nontar, p)
-	sum(nontar .< p)/length(nontar)
-end
-
-function sensitivity(tar, nontar, p)
-	sum(tar .>= p)/length(tar)
-end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using JUtils
+using Test
+
+@testset "JUtils.jl" begin
+    # Write your tests here.
+end
